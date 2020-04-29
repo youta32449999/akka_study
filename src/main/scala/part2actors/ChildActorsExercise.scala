805ecf47ccfe25c8320183a0c938f54a7b1d366e
syo <syo@syonoMacBook-puro.local>
2020-04-29 14:49:13 +0900

    answerを見て自分の解答を修正。textが何wordsかわかるように修正

--- src/main/scala/part2actors/ChildActorsExercise.scala
+++ src/main/scala/part2actors/ChildActorsExercise.scala
@@ -1,6 +1,7 @@
 package part2actors
 
 import akka.actor.{Actor, ActorRef, ActorSystem, Props}
+import akka.pattern.BackoffSupervisor.CurrentChild
 
 object ChildActorsExercise extends App {
 
@@ -8,8 +9,8 @@ object ChildActorsExercise extends App {
 
   object WordCounterMaster {
     case class Initialize(nChildren: Int)
-    case class WordCountTask(text: String)
-    case class WordCountReply(count: Int)
+    case class WordCountTask(id: Int, text: String)
+    case class WordCountReply(id: Int, count: Int)
   }
   class WordCounterMaster extends Actor {
 
@@ -18,43 +19,50 @@ object ChildActorsExercise extends App {
     override def receive: Receive = {
       case Initialize(nChildren) =>
         if(nChildren > 0){
-          val children = (1 to nChildren).map(_ => context.actorOf(Props[WordCounterWorker])).toList
-          context.become(running(children, Nil))
+          val children = (1 to nChildren).map(_ => context.actorOf(Props[WordCounterWorker])).toArray
+          context.become(running(children, 0, 0, Map()))
         }
     }
 
-    def running(children: List[ActorRef], executed: List[ActorRef]): Receive = {
-      case WordCountTask(text) =>
-        children match {
-          case Nil => println(0)
-          case x :: xs => {
-            x ! WordCountTask(text)
-            xs match {
-              case Nil => context.become(running(executed, Nil))
-              case _ => context.become(running(xs, executed :+ x))
-            }
-          }
-        }
-      case WordCountReply(count) => println(count)
+    def running(children: Seq[ActorRef], currentChildId: Int, currentTaskId: Int, senderMap: Map[Int, (ActorRef,String)]): Receive = {
+      case text: String =>
+        children(currentChildId) ! WordCountTask(currentTaskId, text)
+        val nextChildId = (currentChildId + 1) % children.length
+        val newSenderMap = senderMap + (currentTaskId -> (sender(), text))
+        context.become(running(children, nextChildId, currentTaskId + 1, newSenderMap))
+      case WordCountReply(taskId, count) =>
+        val (sender, text) = senderMap(taskId)
+        sender ! (text, count)
     }
   }
   class WordCounterWorker extends Actor {
     import WordCounterMaster._
 
     override def receive: Receive = {
-      case WordCountTask(text) =>
+      case WordCountTask(taskId, text) =>
         println(s"${context.self.path}")
         val count = text.split(" ").length
-        sender() ! WordCountReply(count)
+        sender() ! WordCountReply(taskId, count)
+    }
+  }
+
+  class TestActor extends Actor {
+    import WordCounterMaster._
+
+    override def receive: Receive = {
+      case "go" =>
+        val master = context.actorOf(Props[WordCounterMaster], "wordCounterMaster")
+        master ! Initialize(3)
+        val texts = List("I love Akka", "Scala is super dope", "yes", "me too")
+        texts.foreach(text => master ! text)
+      case (text, count) =>
+        println(s"'$text' is $count words")
     }
   }
 
   val system = ActorSystem("system")
-  val wordCounterMaster = system.actorOf(Props[WordCounterMaster], "wordCountMaster")
-  import WordCounterMaster._
-
-  wordCounterMaster ! Initialize(5)
-  (1 to 7).foreach(_ => wordCounterMaster ! WordCountTask("Akka is awesome"))
+  val testActor = system.actorOf(Props[TestActor], "testActor")
+  testActor ! "go"
 
 
   /*
